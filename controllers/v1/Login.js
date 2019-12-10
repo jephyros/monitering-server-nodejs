5efae49872b39bd00aa8093a9cb4ed57f376f674
cis-WIn <kr.chis@gmail.com>
2019-12-10 16:13:55 +0900

    Signup completed

--- controllers/v1/Login.js
+++ controllers/v1/Login.js
@@ -1,5 +1,5 @@
 "use strict";
-const PROMISE = require("bluebird");
+//const PROMISE = require("bluebird");
 const jwt = require("jsonwebtoken");
 
 const secretObj = require("../../utils/jwt");
@@ -36,54 +36,68 @@ exports.auth_POST = (req, res, next) => {
 }
 
 exports.signup_POST = (req, res, next) => {
-    
+
     let userid = req.body.userid;
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password;
 
-
-
-    //아이디중복체크후저장
-    pool.excuteSql("select userid,username,email from users where userid = ? ", [userid])
-        .then((result) => {
-
-            if (result.length > 0) {
-                res.status(200).json({
-                    resultcode: "D01",
-                    resultmsg: "User ID is duplicated ",
-                    resultdata: ""
-                });
-            } else {
-                //데이터 저장 
-                pool.excuteSql("insert into users "
-                    + " ( userid,username,email,password,inserttime) values( ?,?,?,?, now())"
-                    , [userid,
-                        username,
-                        email,
-                        utils.encryptSHA2(password)
-                    ])
-                    .then((result) => {
-                        logger.info('회원가입 성공 : 아이디 <' + userid + '>');
-                        res.status(200).json({
-                            resultcode: "200",
-                            resultmsg: "success",
-                            resultdata: result
-                        });
-
-                    });
-            }
-        }).catch((err) => {
-            logger.error(err.toString());
-            res.status(500).json({
-                resultcode: "500",
-                resultmsg: "ERROR",
-                resultdata: err.toString()
-            });
+    //아이디 비번 존재여부 체크 
+    if (!req.body.userid || !req.body.password || req.body.userid == "" || req.body.password == "") {
+        res.status(400).json({
+            resultcode: "400",
+            resultmsg: "Bad Request",
+            resultdata: ""
         });
+    } else {
+        saveData();
+    }
 
+    function saveData() {
 
+        //  //아이디중복체크후저장
+        pool.excuteSql("select userid,username,email from users where userid = ? ", [userid])
+            .then((result) => {
+
+                if (result.length > 0) {
+                    res.status(200).json({
+                        resultcode: "D01",
+                        resultmsg: "User ID is duplicated ",
+                        resultdata: ""
+                    });
+                } else {
+                    //데이터 저장 
+                    pool.excuteSql("insert into users "
+                        + " ( userid,username,email,password,inserttime) values( ?,?,?,?, now())"
+                        , [userid,
+                            username,
+                            email,
+                            utils.encryptSHA2(password)
+                        ])
+                        .then((result) => {
+                            logger.info('회원가입 성공 : 아이디 <' + userid + '>');
+                            res.status(200).json({
+                                resultcode: "200",
+                                resultmsg: "success",
+                                resultdata: result
+                            });
+
+                        });
+                }
+            }).catch((err) => {
+                logger.error(err.toString());
+                res.status(500).json({
+                    resultcode: "500",
+                    resultmsg: "ERROR",
+                    resultdata: err.toString()
+                });
+            })
+
+    }
 
 
 
 }
+
+
+
