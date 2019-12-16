ef216a4bca221f496ad3b84890f068d031a837bd
cis-WIn <kr.chis@gmail.com>
2019-12-16 16:01:41 +0900

    401 메세지 변경

--- utils/isAuthenticated.js
+++ utils/isAuthenticated.js
@@ -3,37 +3,39 @@ let jwt = require("jsonwebtoken");
 let secretObj = require("./jwt");
 
 
-module.exports  =  function (req, res, next) {
+module.exports = function (req, res, next) {
     let token = extractToken(req);
     //let decoded = jwt.verify(token, secretObj.secret);
-//    console.log('jwt error >>>>>>>>',decoded)
+    //    console.log('jwt error >>>>>>>>',decoded)
 
-    jwt.verify(token, secretObj.secret, function(err, decoded) {
-        if(err){
-            res.status(401).json({
-                message: "401 Unauthorized Error"
-              })
+    jwt.verify(token, secretObj.secret, function (err, decoded) {
+        if (err) {
+            res.status(200).json({
+                resultcode: "401",
+                resultmsg: "Unauthorized Error",
+                resultdata: ""
+            })
             //return next(err);
-        }else{
-   
-            return next();
-       }    
-     });
+        } else {
 
-    
+            return next();
+        }
+    });
+
+
 
     // if (decoded) {
     //   return next();
-      
+
     // }else{
     //   res.status(403).json({
     //     message: "403 Forbidden Error"
     //   })
     // }
-    
+
     //res.redirect('/login');
     //res.render('login/login.ejs',{loginmessage : '해당메뉴에 권한이 없습니다. 로그인이 필요합니다.'});
-  };
+};
 
 
 function extractToken(req) {
